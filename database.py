e53622e614d85e54750abbb36222ea5dbc87e34f
K.Apoorva reddy <kuntaapoorva62@gmail.com>
2025-09-22 22:27:52 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,19 @@
+import mysql.connector
+from db import info
 
-info = {
+try:
+    mysql.connector.connect (**info)
+    print('connection successful')
+except:
+    print('not able to connect')
+      # this is index.py file 
+      # output:- 
+      # connection successful 
+info={
     'user':'root',
-    'password':'Apoorva@28',
+    'password':'deepu@0811',
     'host':'localhost',
-    'port':'3306'
+    'port':3306
 }
+    # this is db.py file 
+pdbc python
